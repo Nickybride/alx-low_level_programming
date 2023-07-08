46ca071add2136233014203611c468b008afb41b
Nickybride <npoussey@gmail.com>
2023-07-08 02:17:23 -0700

    updated

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr - entry point
+ * @haystack: the string to be searched.
+ * @needle: the substring to search for.
+ * Return: always 0 (success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+
+		if (*p == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
